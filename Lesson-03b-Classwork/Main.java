b00eb58038b5026140b944cf27c997737c9e0640
Liming-L <limingl2789@nycstudents.net>
2025-09-11 14:12:45 +0000

    done

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.printIn("Liming Liu");
+    System.out.printIn("Liming Liu at 6705 152nd st Flushing 11367");
+    System.out.printIn("Liming Liu \n 6705 152nd st Flushing 11367");
+  }
+
+  
+}
