2b7f0e1448c63cd8b7fcb9e33cbdf3481d737f69
Liming-L <limingl2789@nycstudents.net>
2025-09-29 16:59:38 +0000

    Done

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -36,10 +36,8 @@ class Main {
   }
 
    double distance(double x1, double x2, double y1, double y2){
-    double result=(Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
+    double result=Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
     return result;
-
-
   }
  
 
