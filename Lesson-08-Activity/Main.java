c2c65880fc7b66a0c5052f0771a912a4841472ce
Liming-L <limingl2789@nycstudents.net>
2025-09-29 14:02:01 +0000

    awdas

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -6,10 +6,20 @@ class Main {
 
   void init(){
 	print("Hello");
+	System.out.println("enter Fahrenheit");
+    double f= Input.readDouble();
+    double c=0;
+    c=(32*f-32)*(5/9);
+    System.out.println(c);
   }
 
   void print(String msg){
 	System.out.println(msg);
+  }
+
+  void print(String FtoC){
+	System.out.println(FtoC);
+
   }
   
  
