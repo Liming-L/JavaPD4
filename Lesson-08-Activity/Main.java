5562d7640b470bee8e4afdb01dc82362ab7d3dd9
Liming-L <limingl2789@nycstudents.net>
2025-09-29 14:09:58 +0000

    wadasd

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -6,18 +6,17 @@ class Main {
 
   void init(){
 	print("Hello");
-    double f= 98.6;
-    double FtoC=0;
-    FtoC=(f-32)*(5/9);
   }
 
   void print(String msg){
 	System.out.println(msg);
   }
 
-  void print(double FtoC){
-	System.out.println(FtoC);
-
+  void FtoC(double F){
+    double c= 98.6;
+    double FtoC=0;
+    FtoC=(f-32)*(5/9.0);
+	System.out.println(F);
   }
   
  
