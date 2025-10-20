25ad6fd3284b05edf5b2f0405b9e07b642564f3e
Liming-L <limingl2789@nycstudents.net>
2025-10-20 20:33:32 +0000

    Done

--- Lesson-12-Classwork/Main.java
+++ Lesson-12-Classwork/Main.java
@@ -2,6 +2,8 @@ class Main {
 
   public static void main(String[] args) {
     (new Main()).init();
+    System.out.println(collegeCredit("ABC12E"));
+
   }
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
@@ -10,6 +12,17 @@ class Main {
    
 
     
+  }
+
+  boolean collegeCredit(String courseCode){
+    char sixLetter=courseCode.charAt(5);
+    if(sixLetter=='E'||sixLetter=='M'){
+      return true;
+    }
+    else{
+      return false;
+    }
+
   }
 
  
