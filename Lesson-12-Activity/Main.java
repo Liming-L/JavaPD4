b7f3e9d49931de7fe01842c8243746840a55dc39
Liming-L <limingl2789@nycstudents.net>
2025-10-21 14:48:53 +0000

    Done

--- Lesson-12-Activity/Main.java
+++ Lesson-12-Activity/Main.java
@@ -9,7 +9,36 @@ class Main {
   void init(){
    
   }
+  String creditCardType(String card){
+    if(card.substring(0,1).equals("4")){
+      return "Visa";
+    }
+    else if(card.substring(0,2).equals("34")||card.substring(0,2).equals("37")){
+      return "American express";
+    }
+    else if(card.substring(0,2).equals("36")){
+      return "Diner Club";
+    }
+    else if(card.substring(0,2).equals("51")||card.substring(0,2).equals("55")){
+      return "Mastercard";
+    }
+    else if(card.substring(0,4).equals("6011")||card.substring(0,2).equals("65")){
+      return "Discovery";
+    }
+    else{
+      return "Unknown";
+    }
+  }
+  String pigLatin(String word){
+  }
 
-  
-  
+  String nycLocate(){
+    if(word.substring()){
+
+    }
+  }
+
+
+
+
 }
